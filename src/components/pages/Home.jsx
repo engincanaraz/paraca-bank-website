9ce63377a138ba4af53593138983d119590c5590
engincanaraz <engincanaraz@outlook.com>
2025-04-21 06:26:55 +0300

    Ana Sayfaya Blog Bölümü Eklendi, Blog Yazıları İçin DaisyUI Carousel ve Card Yapısı Kullanıldı"

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -7,8 +7,12 @@ import img4 from "../../assets/images/shapes/4.png";
 import img5 from "../../assets/images/shapes/5.png";
 import img6 from "../../assets/images/shapes/6.png";
 import img7 from "../../assets/images/shapes/7.png";
-import arrow from "../../assets/images/icons/arrow-right.png";
+import arrowBlue from "../../assets/images/icons/arrow-right-blue.png";
+import arrowWhite from "../../assets/images/icons/arrow-right-white.png";
 import Background from "../../assets/images/shapes/Background.png";
+import blog1 from "../../assets/images/blog-1.png";
+import blog2 from "../../assets/images/blog-2.png";
+import blog3 from "../../assets/images/blog-3.png";
 
 const Home = () => {
   return (
@@ -17,10 +21,10 @@ const Home = () => {
       <div className="container mx-auto py-10 lg:py-20 px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div className="flex flex-col gap-4">
-            <h1 className="text-[32px] md:text-[52px] lg:text-[72px]  font-bold text-black   lg:pb-5 font-sf-pro-rounded">
+            <h1 className="text-[32px] md:text-[52px] lg:text-[72px]  font-bold text-black   lg:py-20 font-sf-pro-rounded">
               Cebindeki <br /> dijital banka
             </h1>
-            <p className="text-[18px] md:text-[24px] lg:text-[24px] text-black font-medium lg:max-w-[500px]  font-sf-pro-rounded">
+            <p className="text-[18px] md:text-[24px] lg:text-[24px] text-black font-medium lg:max-w-[600px]  font-sf-pro-rounded">
               Aidat yok, masraf yok, zorluk yok! Avantajlı fırsatlarından
               yararlan, geleneksel bankacılığı bir kenara bırak.
             </p>
@@ -28,10 +32,10 @@ const Home = () => {
               to="/signup"
               className="btn rounded-full bg-[#194FDA]/20 border-none text-[#194FDA] font-bold text-[18px] lg:text-[24px] flex items-center gap-2 w-[250px] lg:w-[259px] font-sf-pro-rounded ">
               ParacaCard iste{" "}
-              <img src={arrow} alt="arrow" className="w-[24px] h-[20px]" />
+              <img src={arrowBlue} alt="arrow" className="w-[24px] h-[20px]" />
             </Link>
           </div>
-          <div className="flex flex-col gap-4 relative">
+          <div className="flex flex-col gap-4 relative px-24">
             <img
               src={hand}
               alt="hand"
@@ -179,6 +183,168 @@ const Home = () => {
               </h4>
             </div>
           </div>
+        </div>
+      </div>
+
+      {/* 3. Section - Blog */}
+      <div className="container mx-auto lg:py-10 px-4">
+        <h5 className="text-[18px] md:text-[28px] lg:text-[34px]  lg:max-w-[159px] font-medium text-[#3369C8] pb-5 font-sf-pro-rounded">
+          BLOG
+        </h5>
+        <h1 className="text-[28px] md:text-[42px] lg:text-[64px] text-black font-bold pb-5 lg:pb-20 font-sf-pro-rounded">
+          Takipte kal
+        </h1>
+
+        {/* Masaüstü için Grid, sadece büyük ekranlar (lg:) */}
+        <div className="hidden lg:grid lg:grid-cols-3 gap-4">
+          {/* Blog 1 */}
+          <div className="flex card bg-[#194FDA]/20 w-96 shadow-sm rounded-[20px]">
+            <figure className="p-5">
+              <img src={blog1} alt="blog1" className="rounded-[20px]" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title text-[#EB5757] font-medium text-[18px]">
+                KATEGORİ
+              </h2>
+              <Link to="/blog/nasil-para-gonderebilirim">
+                <h6 className="text-[28px] font-bold text-[#1D1D1D] mb-10 hover:text-[#3369C8] transition-colors">
+                  Nasıl Para Gönderebilirim?
+                </h6>
+              </Link>
+              <div className="card-actions justify-start">
+                <div className="badge badge-outline border-none text-[#1D1D1D] p-0">
+                  25 Ocak 2021
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Blog 2 */}
+          <div className="flex card bg-[#194FDA]/20 w-96 shadow-sm rounded-[20px]">
+            <figure className="p-5">
+              <img src={blog2} alt="blog2" className="rounded-[20px]" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title text-[#EB5757] font-medium text-[18px]    ">
+                KATEGORİ
+              </h2>
+              <Link to="/blog/paraca-ile-harcama-takibi">
+                <h6 className="text-[28px] font-bold text-[#1D1D1D] mb-10 hover:text-[#3369C8] transition-colors">
+                  Paraca ile Harcama <br />
+                  Takibi
+                </h6>
+              </Link>
+              <div className="card-actions justify-start">
+                <div className="badge badge-outline border-none text-[#1D1D1D] p-0">
+                  25 Ocak 2021
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Blog 3 */}
+          <div className="flex card bg-[#194FDA]/20 w-96 shadow-sm rounded-[20px]">
+            <figure className=" p-5">
+              <img src={blog3} alt="blog3" className="rounded-[20px]" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title text-[#EB5757] font-medium text-[18px]    ">
+                KATEGORİ
+              </h2>
+              <Link to="/blog/faturalarimi-otomatik-odeyebilir-miyim">
+                <h6 className="text-[28px] font-bold text-[#1D1D1D] mb-10 hover:text-[#3369C8] transition-colors">
+                  Faturalarımı Otomatik Ödeyebilir Miyim?
+                </h6>
+              </Link>
+              <div className="card-actions justify-start">
+                <div className="badge badge-outline border-none text-[#1D1D1D] p-0">
+                  25 Ocak 2021
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Mobil ve Tablet için Carousel (lg: ekran boyutundan küçük) */}
+        <div className="lg:hidden carousel carousel-center w-full pb-8 space-x-4 rounded-box">
+          {/* Blog 1 - Carousel Item */}
+          <div className="carousel-item">
+            <div className="flex card bg-[#194FDA]/20 w-64 sm:w-72 md:w-96 shadow-sm rounded-[20px]">
+              <figure className="p-5">
+                <img src={blog1} alt="blog1" className="rounded-[20px]" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-[#EB5757] font-medium text-[18px]">
+                  KATEGORİ
+                </h2>
+                <Link to="/blog/nasil-para-gonderebilirim">
+                  <h6 className="text-[20px] md:text-[24px] font-bold text-[#1D1D1D] mb-6 hover:text-[#3369C8] transition-colors">
+                    Nasıl Para Gönderebilirim?
+                  </h6>
+                </Link>
+                <div className="card-actions justify-start">
+                  <div className="badge badge-outline border-none text-[#1D1D1D] p-0">
+                    25 Ocak 2021
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Blog 2 - Carousel Item */}
+          <div className="carousel-item">
+            <div className="flex card bg-[#194FDA]/20 w-64 sm:w-72 md:w-96 shadow-sm rounded-[20px]">
+              <figure className="p-5">
+                <img src={blog2} alt="blog2" className="rounded-[20px]" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-[#EB5757] font-medium text-[18px]">
+                  KATEGORİ
+                </h2>
+                <Link to="/blog/paraca-ile-harcama-takibi">
+                  <h6 className="text-[20px] md:text-[24px] font-bold text-[#1D1D1D] mb-6 hover:text-[#3369C8] transition-colors">
+                    Paraca ile Harcama Takibi
+                  </h6>
+                </Link>
+                <div className="card-actions justify-start">
+                  <div className="badge badge-outline border-none text-[#1D1D1D] p-0">
+                    25 Ocak 2021
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Blog 3 - Carousel Item */}
+          <div className="carousel-item">
+            <div className="flex card bg-[#194FDA]/20 w-64 sm:w-72 md:w-96 shadow-sm rounded-[20px]">
+              <figure className="p-5">
+                <img src={blog3} alt="blog3" className="rounded-[20px]" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-[#EB5757] font-medium text-[18px]">
+                  KATEGORİ
+                </h2>
+                <Link to="/blog/faturalarimi-otomatik-odeyebilir-miyim">
+                  <h6 className="text-[20px] md:text-[24px] font-bold text-[#1D1D1D] mb-6 hover:text-[#3369C8] transition-colors">
+                    Faturalarımı Otomatik Ödeyebilir Miyim?
+                  </h6>
+                </Link>
+                <div className="card-actions justify-start">
+                  <div className="badge badge-outline border-none text-[#1D1D1D] p-0">
+                    25 Ocak 2021
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-center items-center w-full mt-5">
+          <Link
+            to="/blog"
+            className="btn rounded-full bg-transparent border-white border-[2px] text-white font-bold text-[18px] lg:text-[24px] flex items-center justify-center gap-2 w-[300px] lg:w-[410px] font-sf-pro-rounded">
+            Daha fazlasına göz at{" "}
+            <img src={arrowWhite} alt="arrow" className="w-[24px] h-[20px]" />
+          </Link>
         </div>
       </div>
     </div>
